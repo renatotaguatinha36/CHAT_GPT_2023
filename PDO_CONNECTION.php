2309cd7f6fa54270e4e3ee0a86bd336f39248737
renatoguara2020 <renatoguara2020@gmail.com>
2023-05-02 20:27:05 -0300

    ok

--- PDO_CONNECTION.php
+++ PDO_CONNECTION.php
@@ -9,9 +9,10 @@ try {
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   echo "Connected successfully";
-  
+
 } catch(PDOException $e) {
 
   echo "Connection failed: " . $e->getMessage();
+  echo "Connection Failed:" .$e->getTraceAsString();
 }
 ?>
